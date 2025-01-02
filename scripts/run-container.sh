3a356aece1d11b7310bfd53d7a60b078b82b0358
ChunyiWang <wchunyi@foxmail.com>
2025-01-02 16:23:41 +0800

    fix syntax error and bug

--- scripts/run-container.sh
+++ scripts/run-container.sh
@@ -9,17 +9,20 @@ set -u
 : "$VERSION"
 : "$SERVER_USER"
 : "$SERVER_IP"
+: "$REGISTRY_PW"
+: "$REGISTRY_UN"
 
 ssh -p 22990 -T -o StrictHostKeyChecking=no "$SERVER_USER"@"$SERVER_IP" << EOF
 sudo docker stop "$CONTAINER_NAME" || true
 sudo docker rm "$CONTAINER_NAME" || true
+sudo echo "$REGISTRY_PW" | docker login "$CONTAINER_REGISTRY" --username "$REGISTRY_UN" --password-stdin
 sudo docker pull "$CONTAINER_REGISTRY"/"$REGISTRY_NAMESPACE"/wms-backend:"$VERSION"
 if [ $? -ne 0 ]; then
     echo "Failed to pull docker image to the server"
     exit 1
 fi
 sudo docker run -d --name "$CONTAINER_NAME" -p "$EXPOSED_PORT":"$CONTAINER_PORT" "$CONTAINER_REGISTRY"/"$REGISTRY_NAMESPACE"/wms-backend:"$VERSION"
-if [ $? -ne 0]; then
+if [ $? -ne 0 ]; then
     echo "Failed to run docker container"
     exit 1
 fi
