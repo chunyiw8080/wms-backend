7f0617854530babecb181f2b008bbe47578324f3
ChunyiWang <wchunyi@foxmail.com>
2025-01-03 11:45:23 +0800

    update database configurations

--- config/settings.py
+++ config/settings.py
@@ -4,6 +4,6 @@ DB_HOST = os.getenv('DB_HOST', 'default_host')
 DB_USER = os.getenv('DB_USER', 'default_user')
 DB_PASSWORD = os.getenv('DB_PASSWORD', 'default_password')
 DB_NAME = os.getenv('DB_NAME', 'default_db')
-DB_PORT = os.getenv('DB_PORT', 3306)
+DB_PORT = int(os.getenv('DB_PORT', 3306))
 
 TOKEN_SECRET_KEY = os.getenv('TOKEN_SECRET_KEY', 'default_token')
